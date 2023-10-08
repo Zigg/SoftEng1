9b52bae16fe68f31fe8e2e866f4a0df4f9c30556
Daniel Evans <danielevans254@yahoo.com>
2023-10-08 12:07:59 +0800

    Updated register api route

--- app/api/users/register/route.ts
+++ app/api/users/register/route.ts
@@ -1,49 +1,69 @@
 import bcrypt from "bcrypt";
-import prisma from "@/app/libs/prismadb";
+import db from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
+// FIXME: Weird response body in the header, it throws an image null for some reason? The session throws a random image response upon being redirect to /
 export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const {
+
+  const body = await req.json();
+  const {
+    email,
+    username,
+    password,
+  } = body;
+
+  // TODO: I tested this it works, must implement also in the AuthForm, put in api/auth/[...nextauth]. It throws proper email and username already exists toast errors
+
+  // if (!email || !username || !password) {
+  //   return NextResponse.json("Missing required fields", { status: 400 });
+  // }
+
+  const existingUserEmail = await db.user.findUnique({
+    where: {
+      email: email
+    }
+  });
+
+  const existingUserName = await db.user.findUnique({
+    where: {
+      username: username
+    }
+  });
+
+  if (existingUserName) {
+    return NextResponse.json({ user: null, message: "Username already exists" }, { status: 409 });
+  }
+  else if (existingUserEmail) {
+    return NextResponse.json({ user: null, message: "Email already exists" }, { status: 409 });
+  }
+
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  // DONE: Initialize the cart and wallet for the new user
+  const newUser = await db.user.create({
+    data: {
       email,
       username,
-      password,
-    } = body;
-
-    // if (!email || !username || !password) {
-    //   return NextResponse.json("Missing required fields", { status: 400 });
-    // }
-
-    // check if a user with the given email or username already exists
-    const existingUser = await prisma.user.findFirst({
-      where: {
-        OR: [{ email }, { username }],
+      hashedPassword,
+      // TODO: Commented out first as this will result in increased row reads and insert to the database
+      // For initializing the cart and wallet for the new user
+      wallet: {
+        create: {},
       },
-    });
+      cart: {
+        create: {},
+      },
+    },
+    include: {
+      wallet: true,
+      cart: true,
+    },
+  })
 
-    if (existingUser) {
-      if (existingUser.email === email) {
-        return NextResponse.json({ message: "Email already exists" }, { status: 409 });
-      }
-      if (existingUser.username === username) {
-        return NextResponse.json({ message: "Username already exists" }, { status: 409 });
-      }
-    }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        username,
-        hashedPassword
-      }
-    });
+  return NextResponse.json(newUser);
+  // TODO: All appropriate errors will be handle in the next-auth api route
 
-    return NextResponse.json(user);
-  } catch (error: any) {
-    console.log(error, "REGISTER ERROR")
-    return NextResponse.json("Internal Server Error", { status: 500 });
-  }
 }
