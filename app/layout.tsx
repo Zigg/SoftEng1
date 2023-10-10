c708ab3f547484a1bd53e25afdc964dd1b7f434d
Daniel Evans <danielevans254@yahoo.com>
2023-10-11 06:59:40 +0800

    Removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import AuthContext from '@/context/AuthContext'
 import ToasterContext from '@/context/ToasterContext'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Ordering System',
