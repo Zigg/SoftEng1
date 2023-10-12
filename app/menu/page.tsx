e94da5b2de2bebe3deb0fdb24b4f670533840821
Daniel Evans <danielevans254@yahoo.com>
2023-10-13 06:28:52 +0800

    Added Menu Page placeholder

---
+++ app/menu/page.tsx
@@ -0,0 +1,7 @@
+// TODO: This will obviously be within the main dashboard page and rendered as a component or might serve as its own page idk yet
+
+const MenuPage = () => {
+  return <div>Menu Page</div>;
+};
+
+export default MenuPage;
