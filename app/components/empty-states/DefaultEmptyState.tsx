ee828e26f0884c1f0600a2eac8901b986e972b29
Daniel Evans <danielevans254@yahoo.com>
2023-10-13 07:44:17 +0800

    Initialized state for if no tasks/items available

--- app/components/empty-states/DefaultEmptyState.tsx
+++ app/components/empty-states/DefaultEmptyState.tsx
@@ -3,34 +3,16 @@ const DefaultEmptyState = () => {
   return (
     <div
       className="
-        px-4
-        py-10
+        pb-10
         sm:px-6
         h-full
         flex
         justify-center
         items-center
-       
-        "
+      "
     >
-      <div
-        className="
-        text-center
-        items-center 
-        flex 
-        flex-col
-        "
-      >
-        <h3
-          className="
-            mt-2
-            text-2xl
-            font-semibold
-            text-gray-900
-            "
-        >
-          Nothing Found
-        </h3>
+      <div className="flex items-center justify-center">
+        <h3 className="text-2xl font-semibold text-gray-900">Nothing Found</h3>
       </div>
     </div>
   );
