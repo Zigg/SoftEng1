1a832ee4710a46bf416ae15d7dcdf5a38e2c0ed3
Daniel Evans <danielevans254@yahoo.com>
2023-10-17 13:56:23 +0800

    Intialized the app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 // Apparently app doesnt work and has to be App
 import App from "./App";
 
+import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
