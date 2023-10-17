cd53621ea7a7fcf0cdf7cdad9407dd01940aed04
Daniel Evans <danielevans254@yahoo.com>
2023-10-17 13:53:08 +0800

    setup and added functions for testing the api

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,19 +1,34 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const express = require("express");
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const serviceAccountKey = require("./serviceAccountKey.json");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// TODO: Setup up admin credentials for firebase, still checking
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
+
+const app = express();
+
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors({origin: true}));
+
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// TODO: Setup user routes
+// TODO: Check this route
+const userRoute = require("./routes/user");
+app.use("/api/users", userRoute);
+
+exports.app = functions.https.onRequest(app);
