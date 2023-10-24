bde9a70bf10dcd2ab6bcffc4247125b6e452ca2d
Daniel Evans <danielevans254@yahoo.com>
2023-10-24 17:14:52 +0800

    Changed anchor tag to Navlink
    Added pseudo class to change styles of the menu item text when currently within the same route

--- frontend/src/containers/components/main/TopNavbar.jsx
+++ frontend/src/containers/components/main/TopNavbar.jsx
@@ -12,6 +12,8 @@ import {
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router";
 import SearchInput from "./navbar/SearchInput.jsx";
+import { NavLink } from "react-router-dom";
+
 const auth = getAuth();
 const TopNavbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -151,37 +153,54 @@ const TopNavbar = () => {
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="/"
-                  className="block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700 "
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                  isActive 
+                    ? "font-bold text-rose-500 hover:text-blue-600" 
+                    : "block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700"
+                }
                 >
                   🏠Home
-                </a>
+                </NavLink>
               </li>
               <li>
-                <a
-                  href="/menu"
-                  className="block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700 "
+                <NavLink
+                  to="/menu"
+                  className={({ isActive }) =>
+                  isActive 
+                    ? "font-bold text-rose-500 hover:text-blue-600" 
+                    : "block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700"
+                }
                 >
                   🌶️Menu
-                </a>
+                </NavLink>
               </li>
               <li>
-                <a
-                  href="/featured"
-                  className="block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700 "
+                <NavLink
+                  to="/featured"
+                  className={({ isActive }) =>
+                  isActive 
+                    ? "font-bold text-rose-500 hover:text-blue-600" 
+                    : "block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700"
+                }
                 >
                   🔥Featured
-                </a>
+                </NavLink>
               </li>
               <li>
-                <a
-                  href="/contacts"
-                  className="block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700 "
-                >
-                  ☎️Contacts
-                </a>
-              </li>
+              <NavLink
+                to="/contacts"
+                className={({ isActive }) =>
+                  isActive 
+                    ? "font-bold text-rose-500 hover:text-blue-600" 
+                    : "block py-2 text-gray-900 rounded hover:bg-blue-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover-bg-transparent dark:border-gray-700"
+                }
+              >
+                ☎️Contacts
+              </NavLink>
+            </li>
+            
             </ul>
           </div>
         </div>
