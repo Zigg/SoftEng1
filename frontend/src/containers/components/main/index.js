47b9d7b3b23365ecf3ea8ea3175c99d2a178d879
Daniel Evans <danielevans254@yahoo.com>
2023-10-24 15:46:04 +0800

    Add more components

--- frontend/src/containers/components/main/index.js
+++ frontend/src/containers/components/main/index.js
@@ -1,2 +1,4 @@
 export {default as TopNavbar} from './components/main/TopNavbar'
-export {default as Alert} from './components/main/Alert'
+export {default as Alert} from './components/main/navbar/GlobalAlert'
+export {default as SearchInput} from './components/main/navbar/SearchInput'
+export {default as CartBadge} from './components/main/navbar/CartBadge'
