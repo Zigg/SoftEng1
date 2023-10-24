7d1b7416d9b267db4fdfaf770c8767f2c3d9bb93
Daniel Evans <danielevans254@yahoo.com>
2023-10-24 09:23:43 +0800

    Add user avatar img as an export

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -1,3 +1,3 @@
 export {default as LoginBG} from '../images/informational/LoginBG.jpg'
 export {default as Logo} from '../images/informational/Logo.svg'
-
+export {default as defaultUser} from '../images/default/defaultUser.jpg'
