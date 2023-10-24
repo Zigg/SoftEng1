4d36db9dd6a9abc3f0866ca237c605d6f9929bd7
Daniel Evans <danielevans254@yahoo.com>
2023-10-24 09:24:59 +0800

    Add Loading Screen
    Fixed Styles
    
    Initialize Global Alert Toast Notifications for user actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
+import { app } from "./config/firebase.config.js";
+import { useDispatch } from "react-redux";
+import { getAuth } from "firebase/auth";
+
+import { setUserDetails } from "./context/actions/userActions";
 
 // TODO: Set defaults for routing and adding routes
-// TODO: Destructure routes to make adding addional routes easier
+// TODO: Destructure routes to make adding additional routes easier
 import { Main } from "./containers/Main";
 import { Login } from "./containers/Login";
+import Loader from "./components/Loader.jsx";
+import GlobalAlert from "./containers/components/main/GlobalAlert.jsx";
 const App = () => {
+  const firebaseAuth = getAuth(app);
+  const [isLoading, setIsLoading] = React.useState(false);
+  // TODO: Remove verylong css classes from .js and .jsx files, and should just create them as seperate components to make it easier to read and understand each components within the same folder to also allow for reusable components
+  // TODO: checking if the user is logged in or not, then dispatching the user details to the redux store
+  const dispatch = useDispatch();
+  // TODO: This is for the loading screen for the app however this should only be used for loading contents from the redux store, or database, example would be searching, filtering, etc. Not for pages that dont require loading but I can set it to all pages for now
+  useEffect(() => {
+    setIsLoading(true);
+    const user = firebaseAuth.currentUser;
+    if (user) {
+      console.log(user);
+      dispatch(setUserDetails(user));
+    }
+    setInterval(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, [dispatch, firebaseAuth.currentUser]);
+
+  // TODO: Fix the loading screen styles
   return (
-    <div className="w-screen min-h-screen h-auto flex flex-col items-center justify-center">
-    <Toaster />
+    <div className="">
+      {isLoading && <Loader />}
+
+      <Toaster />
       <Routes>
         <Route path="/*" element={<Main />} />
         <Route path="/login" element={<Login />} />
       </Routes>
       
+      <GlobalAlert type={"info"} message={""}/>
     </div>
   );
 };
