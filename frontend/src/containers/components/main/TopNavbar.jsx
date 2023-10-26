210b304e42895a285c963525349191e1b8eb9e7c
Daniel Evans <danielevans254@yahoo.com>
2023-10-26 13:01:02 +0800

    Add state variables from the global store for username and email

--- frontend/src/containers/components/main/TopNavbar.jsx
+++ frontend/src/containers/components/main/TopNavbar.jsx
@@ -22,6 +22,10 @@ const TopNavbar = () => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
 
+  const userEmail = useSelector(state => state.user && state.user.email ? state.user.email : '');
+
+  const userName = useSelector(state => state.user && state.user.providerData && state.user.providerData[0] ? state.user.providerData[0].displayName : '');
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -36,8 +40,7 @@ const TopNavbar = () => {
   return (
     <div className="md:border-b-2 md:border-gray-300 lg:border-b-2 lg:border-gray-300">
       <nav className="bg-white dark:bg-gray-900 sticky top-0 z-10">
-      <div className="flex flex-wrap items-center justify-between mx-auto p-4 xs:items-center xs:justify-center ">
-
+        <div className="flex flex-wrap items-center justify-between mx-auto p-4 xs:items-center xs:justify-center ">
           <NavLink to="/" className="flex items-center ">
             <img src={Logo} className="h-12 w-12 mr-2" alt="Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ">
@@ -83,90 +86,95 @@ const TopNavbar = () => {
             </button>
 
             {isMenuOpen && (
-              <div
-                className="absolute md:ml-52 md:mt-12  translate-y-1/2 xs:ml-52 xs:mt-12 sm:ml-60 sm:mt-12 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 border-t-2  border-rose-500 dark:border-gray-600"
-                id="user-dropdown"
-              >
-                <div className="px-4 py-3">
-                  <span className="block text-sm text-gray-900 dark:text-white font-medium">
-                    Test User
-                  </span>
-                  <span className="block text-sm text-gray-500 truncate dark:text-gray-400 mb-1 font-medium">
-                    test@email.com
-                  </span>
-                  <span className="block text-sm text-black-500 truncate dark:text-white font-medium">
-                    Balance: ₱110.42
-                    {/* TODO: Replace this with the actual value  */}
-                  </span>
-                </div>
-                <ul className="py-2" aria-labelledby="user-menu-button">
-                  <li>
-                    <NavLink
-                      to="profile"
-                      className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      <TbUser className="mr-2" />
-                      Profile
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="order"
-                      className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      <TbPackage className="mr-2" />
-                      Orders
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="order-history"
-                      className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      <TbHistory className="mr-2" />
-                      History
-                    </NavLink>
-                  </li>
+              
+                <div
+                  className="absolute md:ml-52 md:mt-12  translate-y-1/2 xs:ml-52 xs:mt-12 sm:ml-60 sm:mt-12 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 border-t-2  border-rose-500 dark:border-gray-600"
+                  id="user-dropdown"
+                >
+                <div>
+                  <div className="px-4 py-3">
+                    <span className="block text-sm text-gray-900 dark:text-white font-medium">
+                      
+                      {userName}
+                      {/* TODO: Replace this with the actual value  */}
+                    </span>
+                    <span className="block text-sm text-gray-500 truncate dark:text-gray-400 mb-1 font-medium">
+                    {userEmail}
+                      {/* TODO: Replace this with the actual value  */}
+                    </span>
+                    <span className="block text-sm text-black-500 truncate dark:text-white font-medium">
+                      Balance: ₱110.42
+                      {/* TODO: Replace this with the actual value  */}
+                    </span>
+                  </div>
 
-                  <li>
-                    <NavLink
-                      to="transactions"
-                      className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      <TbArrowsTransferDown className="mr-2" />
-                      Transactions
-                    </NavLink>
-                  </li>
-                  <li>
-                    <button
-                      onClick={() => {
-                        auth
-                          .signOut()
-                          .then(() => {
-                            console.log("User signed out");
-                            toast("Bye for now", {
-                              icon: "👋",
+                  <ul className="py-2" aria-labelledby="user-menu-button">
+                    <li>
+                      <NavLink
+                        to="profile"
+                        className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        <TbUser className="mr-2" />
+                        Profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="order"
+                        className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        <TbPackage className="mr-2" />
+                        Orders
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="order-history"
+                        className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        <TbHistory className="mr-2" />
+                        History
+                      </NavLink>
+                    </li>
+
+                    <li>
+                      <NavLink
+                        to="transactions"
+                        className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-blue-400 dark:hover:bg-blue-700 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        <TbArrowsTransferDown className="mr-2" />
+                        Transactions
+                      </NavLink>
+                    </li>
+                    <li>
+                      <button
+                        onClick={() => {
+                          auth
+                            .signOut()
+                            .then(() => {
+                              console.log("User signed out");
+                              toast("Bye for now", {
+                                icon: "👋",
+                              });
+                            })
+                            .catch((error) => {
+                              console.error("Error signing out: ", error);
+                              toast.error("Something went wrong");
                             });
-                          })
-                          .catch((error) => {
-                            console.error("Error signing out: ", error);
-                            toast.error("Something went wrong");
-                          });
-                      }}
-                      className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-rose-500 dark:hover:bg-rose-700 dark:text-gray-200 dark:hover:text-white focus:outline-none w-full"
-                    >
-                      <PiSignOutBold className="mr-2" />
-                      Sign out
-                    </button>
-                  </li>
-                </ul>
+                        }}
+                        className="flex items-center px-4 py-2 text-sm text-black font-medium hover:bg-rose-500 dark:hover:bg-rose-700 dark:text-gray-200 dark:hover:text-white focus:outline-none w-full"
+                      >
+                        <PiSignOutBold className="mr-2" />
+                        Sign out
+                      </button>
+                    </li>
+                  </ul>
+                </div>
               </div>
             )}
           </div>
-          <div
-            className="w-full md:flex md:w-auto md:order-1"
-            id="navbar-user"
-          >
+
+          <div className="w-full md:flex md:w-auto md:order-1" id="navbar-user">
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
