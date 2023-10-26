ef95ac71cb148a4c1c958e589c8bc6d9a2f12000
Daniel Evans <danielevans254@yahoo.com>
2023-10-26 13:01:15 +0800

    Update Styles

--- frontend/src/containers/components/main/navbar/SearchInput.jsx
+++ frontend/src/containers/components/main/navbar/SearchInput.jsx
@@ -4,15 +4,17 @@ export default function SearchInput() {
       <label htmlFor="simple-search" className="sr-only">
         Search
       </label>
-      <div className="relative w-full">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+      <div className="relative ">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none "></div>
+        {/* TODO: I want the button to be absolute however there are some coflicting styles that causes the absolute button to not take up the requried space */}
         <input
-          type="text"
-          id="simple-search"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search food item..."
-          required
-        />
+        type="text"
+        id="simple-search"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 flex-grow"
+        placeholder="Search food item..."
+        required
+    />
+    
       </div>
       <button
         type="submit"
