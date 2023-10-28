cdb08373a64f2a1d17e8192522620c679b1f9768
Daniel Evans <danielevans254@yahoo.com>
2023-10-28 17:14:30 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,12 @@ import ReactDOM from "react-dom/client";
 // Apparently app doesnt work and has to be App
 import App from "./App";
 import rootReducer from "./context/reducers";
-// createStore is deprecated?
-// TODO: Might just use zustand instead idk yet
 // TODO: configureStore might use different functions but I will see if I can change the store to use configureStore
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// Added the redux dev tool extension support
 const rootStore = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
