20d37bf449a5d3c6f8a3dff47773f4506ecefc83
Daniel Evans <danielevans254@yahoo.com>
2023-10-30 16:37:58 +0800

    Updated Styles

--- frontend/src/containers/components/dashboard/components/DashboardHeader.jsx
+++ frontend/src/containers/components/dashboard/components/DashboardHeader.jsx
@@ -1,9 +1,13 @@
 // TODO: Setup the admin role
+// TODO: Add the admin user profile
 import { useLocation, useNavigate } from "react-router-dom";
 import NotificationBell from "./NotificationBell";
 import { PiSignOutBold } from "react-icons/pi";
 import { getAuth } from "firebase/auth";
 import { toast } from "react-hot-toast";
+import { Badge } from "flowbite-react";
+import { Lock } from "lucide-react";
+
 const DashboardHeader = () => {
   // Getting the current route path last sub route
   const navigate = useNavigate();
@@ -16,35 +20,46 @@ const DashboardHeader = () => {
     lastPart.charAt(0).toUpperCase() + lastPart.slice(1);
 
   return (
-    <div className=" flex flex-wrap justify-between p-4">
+    <div className="flex flex-wrap justify-between">
       <div className="font-bold text-2xl">{properCaseRouteLastWord} Page</div>
-      <div className="flex ">
-        <div className="">
+
+      <div className="flex flex-col items-center">
+        <div className="flex ">
           <NotificationBell />
-          
+          <div>
+            <Badge
+              color="green"
+              icon={Lock}
+              className="items-center justify-center"
+            >
+              <p>Admin</p>
+            </Badge>
+          </div>
         </div>
 
         <button
-            onClick={() => {
-              auth
-                .signOut()
-                .then(() => {
-                  console.log("User signed out");
-                  navigate("/login", { replace: true });
-                  toast("Bye for now", {
-                    icon: "👋",
-                  });
-                })
-                .catch((error) => {
-                  console.error("Error signing out: ", error);
-                  toast.error("Something went wrong");
+          onClick={() => {
+            auth
+              .signOut()
+              .then(() => {
+                console.log("User signed out");
+                navigate("/login", { replace: true });
+                toast("Bye for now", {
+                  icon: "👋",
                 });
-            }}
-            className="flex item-center opacity-80 justify-center text-gray-900 rounded-lg border-spacing-2 dark:text-white px-2 hover:text-rose-600"
-          >
-            <PiSignOutBold className="w-6 h-6  " />
-            <span className="px-1 ">Logout</span>
-          </button>
+              })
+              .catch((error) => {
+                console.error("Error signing out: ", error);
+                toast.error("Something went wrong");
+              });
+          }}
+          className="ml-10 flex item-center opacity-80 justify-center text-gray-900 rounded-lg border-spacing-2 dark:text-white p-2 hover:text-rose-600"
+        >
+          <div className="flex items-center justify-center">
+            <PiSignOutBold className="w-4 h-4" />
+            <span className="px-1">Logout</span>
+          </div>
+        </button>
       </div>
     </div>
   );
