590b8d6ce922aed2744e25b6a88436e89c507887
Daniel Evans <danielevans254@yahoo.com>
2023-10-31 10:00:29 +0800

    Updated Styles
    Fixed Pagination

--- frontend/src/containers/components/dashboard/pages/DashboardUsers.jsx
+++ frontend/src/containers/components/dashboard/pages/DashboardUsers.jsx
@@ -133,11 +133,15 @@ const DashboardUsers = () => {
           <li>
             <a
               href="#"
-              onClick={() => handlePageChange(activePage - 1)}
-              className={`flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 ${
+              onClick={() => {
+                if (activePage > 1) {
+                  handlePageChange(activePage - 1);
+                }
+              }}
+              className={`flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-700 bg-white border border-gray-300 rounded-l-lg ${
                 activePage === 1
-                  ? "hover:bg-gray-300 dark:hover:bg-gray-700 dark:hover-text-white"
-                  : ""
+                  ? " bg-blue-600  dark:text-white"
+                  : "hover:bg-gray-200 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark-hover-text-white"
               }`}
             >
               <span className="sr-only">Previous</span>
@@ -165,10 +169,10 @@ const DashboardUsers = () => {
                 <a
                   href={`#page-${i + 1}`}
                   onClick={() => handlePageChange(i + 1)}
-                  className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 ${
+                  className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border  border-gray-300 ${
                     i + 1 === activePage
-                      ? "bg-blue-600 text-white"
-                      : " hover:text-gray-700 hover:bg-gray-300 dark:hover:bg-gray-700 dark:hover-text-white"
+                      ? "text-rose-700 font-semibold dark:text-white"
+                      : "hover:text-gray-700 hover:bg-blue-300 dark-hover-bg-blue-700 dark-hover-text-white"
                   }`}
                 >
                   {i + 1}
@@ -179,11 +183,15 @@ const DashboardUsers = () => {
           <li>
             <a
               href="#"
-              onClick={() => handlePageChange(activePage + 1)}
-              className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 rounded-r-lg bg-white border border-gray-300 ${
+              onClick={() => {
+                if (activePage < Math.ceil(userList.length / itemsPerPage)) {
+                  handlePageChange(activePage + 1);
+                }
+              }}
+              className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 rounded-r-lg bg-white border text-gray-700 border-gray-300 ${
                 activePage === Math.ceil(userList.length / itemsPerPage)
-                  ? "dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-                  : "hover-bg-gray-100 hover:text-gray-700 dark-hover-bg-gray-700 dark-hover-text-white"
+                  ? " dark:bg-blue-800 dark:border-gray-700 dark:text-gray-400"
+                  : "hover:bg-gray-200 hover:text-gray-700 dark-hover-bg-blue-700 dark-hover-text-white"
               }`}
             >
               <span className="sr-only">Next</span>
