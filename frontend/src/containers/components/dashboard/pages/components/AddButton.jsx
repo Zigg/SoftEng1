d80e38105215f9366c8a991cddb4ea1a37b88da1
Daniel Evans <danielevans254@yahoo.com>
2023-11-03 21:24:49 +0800

    Add path props

--- frontend/src/containers/components/dashboard/pages/components/AddButton.jsx
+++ frontend/src/containers/components/dashboard/pages/components/AddButton.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import { Button } from "flowbite-react";
 import { Plus } from "lucide-react";
 
-export function AddButton({ message }) {
+export const AddButton = ({ message, path }) => {
   return (
-    <div className="justify-end px-4 flex-shrink-0 ">
-      <Button color="indigo">
+    <Link to={path}>
+      <Button color="indigo" className="ml-4 mt-2">
         <Plus className="mr-2 h-4 w-4" />
         Add a {message}
       </Button>
-    </div>
+    </Link>
   );
-}
+};
