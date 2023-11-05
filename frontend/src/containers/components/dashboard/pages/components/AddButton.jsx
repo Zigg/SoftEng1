a1eeebb06719b76dca8c88a86257bcd028454cc2
Daniel Evans <danielevans254@yahoo.com>
2023-11-05 08:33:55 +0800

    Update Styles

--- frontend/src/containers/components/dashboard/pages/components/AddButton.jsx
+++ frontend/src/containers/components/dashboard/pages/components/AddButton.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
-import { Button } from "flowbite-react";
-import { Plus } from "lucide-react";
+import { Link } from 'react-router-dom';
+import { Button } from 'flowbite-react';
+import { Plus } from 'lucide-react';
 
 export const AddButton = ({ message, path }) => {
   return (
     <Link to={path}>
-      <Button color="indigo" className="flex-shrink-0 ml-4">
-        <Plus className="mr-2 h-4 w-4 flex-shrink-0" />
+      <Button color="indigo" className="flex-shrink-0 ml-4 ">
+        <Plus className="mr-2 h-4 w-4 flex-shrink-0 xs:hidden" />
         Add a {message}
       </Button>
     </Link>
