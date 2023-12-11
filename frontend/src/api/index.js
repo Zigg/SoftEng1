91d6da764e078382e2ef98001ff528b4b3f95d13
danielevans254 <danielevans254@yahoo.com>
2023-12-12 07:22:40 +0800

    create api endpoint for frontend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -21,4 +21,25 @@ export const getUserList = async () => {
   }
 };
 
+export const addNewProduct = async (productData) => {
+  try {
+    const res = await axios.post(`${baseURL}/api/products/create`, productData);
+    return res.data.productId;
+  } catch (err) {
+    return null;
+  }
+}
+
+export const getAllProducts = async () => {
+  try {
+    const res = await axios.get(`${baseURL}/api/products/all`);
+    console.log(res.data.data);
+    return res.data.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+
 
