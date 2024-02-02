2486a6e32dc66b1aaff783ef615eb7dd24eb86d9
danielevans254 <danielevans254@yahoo.com>
2024-02-02 21:40:36 +0800

    feat(api): updated api endpoints and path params

--- backend/functions/routes/api/cart.js
+++ backend/functions/routes/api/cart.js
@@ -1,22 +1,22 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-len */
+/* eslint-disable new-cap */
 
 const router = require("express").Router();
-const admin = require("firebase-admin");
-
 const cartController = require("../../controllers/cartController");
 
 router.get("/", cartController.cartTestRouteServer);
 router.get("/:cartId", cartController.getUserCartServer);
-router.get("/all/:cartId", cartController.getAllCartItemsServer);
 
-router.post("/create", cartController.createCartServer);
+router.post("/create/:userId", cartController.createCartServer);
 router.post("/add/:cartId", cartController.addToCartServer);
 
 router.patch("/update/:cartId/:productId/:productIdentifier", cartController.changeCartItemQuantityServer);
+// TODO: This will be invoked upon changing product options, i.e change size, addons, etc.
+// router.patch("/update/:cartId/:productId/:productIdentifier", cartController.changeCartItemServer);
 
 router.put("clear/:cartId", cartController.clearCartItemsServer);
 
-router.delete("/delete/:cartId/:productId/:productIdentifier", cartController.clearCartItemsServer);
+router.delete("/delete/:cartId/:productId/:productIdentifier", cartController.deleteCartItemServer);
 
 module.exports = router;
