0c947b7aae3380cdc70bfa39df851749575e83b9
danielevans254 <danielevans254@yahoo.com>
2024-02-04 11:07:44 +0800

    fix(bug): Fix bug from total price calculation not showing proper values if the initial items array is empty
    fix(status code): Properly assigned incorrect status code to match the proper http status code
    
    feat(add to cart action): Add functionality with add to cart to check for existing product id and identifier, if they is a match the item being currently added will merge with the item and add the quantity of the current item and the new item
    
    feat(update cart quantity action): Add update cart quantity action, based on the product Id and identifier, upon updating the quantity the totalPrice is recalculated
    
    feat(delete item from cart): Delete cart item from cart, based on the product Id and identifier, total price is recalculated
    
    feat(todo): Add item to cart needs to account for the product identifier, the total price must be derived from the products doc not the request body, but for testing this is the approach for now

--- backend/functions/controllers/cartController.js
+++ backend/functions/controllers/cartController.js
@@ -22,8 +22,12 @@ const cartTestRouteServer = (_req, res, next) => {
 // TODO: Verify products existence,
 // TODO: Calculate the total price of the items in the cart
 
+// FIXME: If the array is initially empty the price doesn't display the updated price
 const calculateTotalPrice = (items) => {
   let totalPrice = 0;
+  if (items.length === 0) {
+    return totalPrice;
+  }
   items.forEach((item) => {
     const { productPrice, productQuantity } = item;
     if (typeof productPrice === "number" && typeof productQuantity === "number") {
@@ -33,54 +37,94 @@ const calculateTotalPrice = (items) => {
   return Number(totalPrice.toFixed(2));
 };
 
+// REVIEW: This finally adds products with same id's and identifiers
+// NOTE: The product identifier still hasn't yet been assigned to this and must derived from the chosen options
 const addToCartServer = async (req, res) => {
-  // REVIEW:
-  // TODO: Fetch the price based on the products chosen options, or just calculate the price based on the base price of the product, and create a product identifier based on the chosen options
   const id = req.params.cartId;
   try {
+    // NOTE: This can just be set as a helper function
     const cartDoc = await cartCollectionRef.doc(id).get();
     if (!cartDoc.exists) {
       return res.status(404).send({ success: false, msg: `CART NOT FOUND [SERVER]` });
     }
+
+    // NOTE: This can just be set as a helper function
     if (!id || typeof id !== "string") {
       return res.status(400).send({ success: false, msg: "Invalid ID parameter" });
     }
 
+    // NOTE: This can just be set as a helper function
     if (!req.body || !req.body.items || !Array.isArray(req.body.items) || req.body.items.length === 0) {
-      return res.status(400).send({ success: false, msg: "CART ADD CANNOT ADD EMPTY REQUEST [SERVER]" });
+      return res.status(404).send({ success: false, msg: "CART ADD CANNOT ADD EMPTY REQUEST [SERVER]" });
     }
-    const productIds = req.body.items.map((item) => { return item.productId });
 
+    // NOTE: Try to make this a function
+    const productIds = req.body.items.map((item) => {
+      return item.productId
+    });
+
+    // NOTE: This can just be set as a helper function
     const productExists = await Promise.all(productIds.map(checkProductExists));
     if (productExists.includes(false)) {
-      return res.status(400).send({ success: false, msg: "PRODUCT DOES NOT EXIST [SERVER]" });
+      return res.status(404).send({ success: false, msg: "PRODUCT DOES NOT EXIST [SERVER]" });
     }
+
+    // TODO: Instead of assigning the product price in the request body assign it by looking through the products doc
+    // NOTE: This is more annoying than i thought the price in the products are only based prices and increase based on the chosen options, i'll account this later, it shouldn't be mapped from the request body and should be derived from the products doc
+    const productItemPrice = req.body.items.map((item) => {
+      return item.productPrice
+    });
 
     // TODO: Create a product identifier based on the chosen options
     const productDoc = await productsCollectionRef.doc(req.body.items[0].productId).get();
-    // NOTE: I want the index to be mapped based on the options that the user has chosen, and then create a product identifier based on the chosen options, and then check if the product identifier already exists in the cart, if it does then just increment the quantity of the product based on the given quantity (frontend)
-    console.log(productDoc.data().addons[1].name);
-    console.log(productDoc.data().sizes[0].name);
+    // console.log(productDoc.data().addons[1].name);
+    // console.log(productDoc.data().sizes[0].name);
+    // console.log(productDoc.data().sizes[0].name);
+    console.log(productDoc.data(), "Product Doc")
+    // console.log(productDoc.data().basePrice, "Product Price")
 
     const { userId, items: existingItems } = cartDoc.data();
 
     const newItems = (req.body.items || []).map((item) => {
       return {
         productId: item.productId,
+        // FIXME: This should be derived from the added product to cart...
         productIdentifier: item.productIdentifier,
         productQuantity: item.productQuantity,
         productPrice: item.productPrice,
       };
     });
 
-    const updatedItems = [...existingItems, ...newItems];
-    const totalPrice = parseFloat(calculateTotalPrice(updatedItems));
+    // If the item doesn't exists in the cart yet, filter it out and add a new item to the array
+    const newItemsToAdd = newItems.filter((newItem) => {
+      return !existingItems.some((existingItem) => existingItem.productId === newItem.productId && existingItem.productIdentifier === newItem.productIdentifier);
+    });
+
+    // If the item already exists in the items array just reduce the existing items to a new array with updated quantities.
+    const updatedItems = existingItems.reduce((acc, existingItem) => {
+      const newItem = newItems.find((newItem) => newItem.productId === existingItem.productId && newItem.productIdentifier === existingItem.productIdentifier);
+      if (newItem) {
+        // Update the quantity of the existing item
+        existingItem.productQuantity += newItem.productQuantity;
+      }
+      // Add the existing item
+      acc.push(existingItem);
+      return acc;
+    }, []);
+
+    // Concatenate the new items to add with the updated items
+    const finalItems = updatedItems.concat(newItemsToAdd);
+    console.log("Final Items:", finalItems)
+    console.log("New Items to Add", newItemsToAdd)
+
+
+    const totalPrice = parseFloat(calculateTotalPrice(finalItems));
 
     req.body.totalPrice = totalPrice;
 
     // FIXME: This should be an array of objects
     // FIXME: Validate the items array
-    const { error, value } = cartSchema.validate({ ...req.body, userId: userId, items: updatedItems, totalPrice: totalPrice, cartId: id });
+    const { error, value } = cartSchema.validate({ ...req.body, userId: userId, items: finalItems, totalPrice: totalPrice, cartId: id });
     if (error) {
       console.error(`VALIDATION ERROR: ${error.message}`);
       return res.status(400).send({ success: false, msg: `VALIDATION ERROR: ${error.message}` });
@@ -89,10 +133,9 @@ const addToCartServer = async (req, res) => {
         ...value,
       };
 
-      cartCollectionRef.doc(id).update(cart).then(() => {
+      cartCollectionRef.doc(id).set(cart, { merge: true }).then(() => {
         return res.status(200).send({ success: true, data: cart });
       }).catch((error) => {
-        console.error("Error updating document: ", error);
         return res.status(400).send({ success: false, msg: `ADD TO CART ERROR [SERVER] ${error.message}` });
       });
     }
@@ -106,83 +149,73 @@ const addToCartServer = async (req, res) => {
 // --Same items but different options are separated, product identifier will be created based on the selected options
 // --Adding a product to the cart whilst there already is an instance of the items will add x quantity from the current items selected
 // TODO:
-// FIXME: This is not yet implemented
-const changeCartItemQuantityServer = async (req, res, next) => {
+// REVIEW:
+const updateCartItemQuantityServer = async (req, res, next) => {
   try {
-    const id = req.params.cartId;
-    if (!id || typeof id !== "string") {
+    const cartId = req.params.cartId;
+    const productId = req.params.productId;
+    const productIdentifier = req.body.productIdentifier;
+    const productQuantity = req.body.productQuantity;
+
+    if (!cartId || typeof cartId !== "string" || !productId || typeof productId !== "string") {
       return res.status(400).send({ success: false, msg: "Invalid ID parameter" });
     }
-    console.log(id);
-    const { error, value } = cartSchema.validate(req.body);
 
-    if (error) {
-      return res.status(400).send({ success: false, msg: `VALIDATION ERROR: ${error.message}` });
-    } else {
-      const cart = {
-        ...value,
-      };
+    // NOTE: This can just be set as a helper function
+    const cartDoc = await cartCollectionRef.doc(cartId).get();
+    if (!cartDoc.exists) {
+      return res.status(404).send({ success: false, msg: "Cart not found" });
+    }
 
-      cartCollectionRef.add(cart).then((docRef) => {
-        return res.status(200).send({ success: true, data: cart, id: docRef.id });
-      }).catch((error) => {
-        console.error("Error adding document: ", error);
-        return res.status(400).send({ success: false, msg: `CHANGE CART ITEM QUANTITY ERROR [SERVER] ${error.message}` });
+    if (!productIdentifier || typeof productIdentifier !== "string") {
+      return res.status(404).send({ success: false, msg: "Missing product identifier" });
+    }
+
+    // Check if the product exists and if not automatically remove from cart
+    const productDoc = await productsCollectionRef.doc(productId).get();
+    if (!productDoc.exists) {
+      // Remove the item from the cart if the product doesn't exist
+      await cartCollectionRef.doc(cartId).update({
+        items: admin.firestore.FieldValue.arrayRemove({ productId, productIdentifier }),
       });
+      return res.status(200).send({ success: true, msg: "Product not found, item removed from cart" });
     }
+
+    // Check if the item with the given productIdentifier and productId exists in the cart
+    const cartItems = cartDoc.data().items;
+    const itemIndex = cartItems.findIndex((item) => item.productId === productId && item.productIdentifier === productIdentifier);
+    if (itemIndex === -1) {
+      return res.status(404).send({ success: false, msg: "Item not found in the cart" });
+    }
+
+    // Update the quantity of the cart item
+    const item = cartItems[itemIndex];
+    item.productQuantity = productQuantity <= 0 ? 1 : productQuantity;
+
+    // Update the cart item in the cart
+    cartItems[itemIndex] = item;
+    await cartCollectionRef.doc(cartId).update({ items: cartItems });
+
+    const totalPrice = calculateTotalPrice(cartItems);
+    await cartCollectionRef.doc(cartId).update({ totalPrice });
+    return res.status(200).send({ success: true, msg: "Cart item quantity updated", productQuantity: item.productQuantity, totalPrice: totalPrice });
   } catch (error) {
-    return res.status(400).send({ success: false, msg: `CHANGE CART ITEM QUANTITY ERROR [SERVER] ${error.message}` });
-  }
-};
-
-// TODO: Clear cart once a checkout session is made a completed successfully, maybe send these items to previously bought items
-// TODO:
-// FIXME: This is not yet implemented
-// REVIEW:
-const clearCartItemsServer = async (req, res, next) => {
-  try {
-    const id = req.params.cartId;
-    if (!id || typeof id !== "string") {
-      return res.status(400).send({ success: false, msg: "Invalid ID parameter" });
-    }
-    const doc = await cartCollectionRef.doc(id).get();
-
-    if (!doc.exists) {
-      return res.status(404).send({ success: false, msg: `CART NOT FOUND [SERVER]` });
-    } else {
-      const getUserCartResponse = await getUserCartServer(req, res, next);
-
-      if (!getUserCartResponse.success) {
-        return res.status(400).send({ success: false, msg: getUserCartResponse.msg });
-      }
-
-      // Clear the items within the cart
-      const cart = {
-        ...getUserCartResponse.data,
-        // REVIEW:
-        items: [{}],
-      };
-      console.log(cart);
-
-      await cartCollectionRef.doc(id).update(cart);
-      return res.status(200).send({ success: true, msg: "Cart items cleared successfully" });
-    }
-  } catch (error) {
-    console.error(`CLEAR CART ITEMS ERROR [SERVER] ${error.message}`);
-    return res.status(400).send({ success: false, msg: `CLEAR CART ITEMS ERROR [SERVER] ${error.message}` });
+    console.error(`UPDATE CART ITEM QUANTITY ERROR [SERVER] ${error.message}`);
+    return res.status(400).send({ success: false, msg: `UPDATE CART ITEM QUANTITY ERROR [SERVER] ${error.message}` });
   }
 };
 
 // REVIEW:
-// TODO: Add middleware to check for the owner of the cart
-// TODO: This works but change it to fetch cart id instead
+// TODO: Add middleware to check for the owner of the cart or admin, if not the user cannot view that cart
 const getUserCartServer = async (req, res, next) => {
+  const id = req.params.cartId;
   try {
-    const id = req.params.cartId;
+    // NOTE: This can just be set as a helper function
     if (!id || typeof id !== "string") {
       return res.status(400).send({ success: false, msg: "Invalid ID parameter" });
     }
 
+    // NOTE: This can just be set as a helper function
     const doc = await cartCollectionRef.doc(id).get();
     if (!doc.exists) {
       return res.status(404).send({ success: false, msg: "CART NOT FOUND [SERVER]" });
@@ -192,15 +225,15 @@ const getUserCartServer = async (req, res, next) => {
     const totalPrice = calculateTotalPrice(items);
 
     req.body.totalPrice = parseFloat(totalPrice);
-    const { error, value } = cartSchema.validate({ items, userId, totalPrice });
-    console.log(value)
+    const { error, value } = cartSchema.validate({
+      items, userId, totalPrice, cartId: doc.id,
+    });
     if (error) {
       console.error(`VALIDATION ERROR: ${error.message}`);
       return res.status(400).send({ success: false, msg: `VALIDATION ERROR: ${error.message}` });
     }
 
     const response = {
-      cartId: doc.id,
       ...value,
     };
 
@@ -212,42 +245,35 @@ const getUserCartServer = async (req, res, next) => {
 };
 
 // REVIEW:
-// TODO: The items nested document must be an array of objects with the following properties: productId, productIdentifier, productQuantity, productPrice, and must be made upon invoking the create cart api endpoint
 // NOTE: This can actually just be a helper function and can be invoked upon creation of an account, but for now just leave it here
 const createCartServer = async (req, res, next) => {
-  // REVIEW: This is finally getting the uuid from firebase authentication, however further testing is needed to check whether it is working properly
   try {
     const user = await admin.auth().getUser(req.params.userId);
     console.log(user.uid);
     const userId = user.uid;
     req.params.userId = userId;
 
-    if (!userId) {
-      return res.status(400).send({ success: false, msg: "User UUID not found" });
-    }
+    // NOTE: This can just be set as a helper function
     if (!userId || typeof userId !== "string") {
       return res.status(400).send({ success: false, msg: "Invalid ID parameter" });
     }
 
-    // NOTE: Initialize the empty array of objects
-    const items = [{}];
-    const { error, value } = createCartSchema.validate({ ...req.body, userId: userId, items: items });
+    const userCartRef = await cartCollectionRef.where("userId", "==", userId).limit(1).get();
+    if (!userCartRef.empty) {
+      const cartId = userCartRef.docs[0].id;
+      return res.status(409).send({ success: true, msg: "User already has a cart", cartId: cartId });
+    }
 
+    // NOTE: Initialize the empty array of objects
+    const items = [];
+    const { error, value } = createCartSchema.validate({ ...req.body, userId: userId, items: items });
+    console.log(value)
     if (error) {
       console.error(`VALIDATION ERROR: ${error.message}`);
       return res.status(400).send({ success: false, msg: `VALIDATION ERROR: ${error.message}` });
     }
 
-    // TODO: Check if user already has a cart document assigned
-    const userCartRef = await cartCollectionRef.where("userId", "==", userId).limit(1).get();
-    if (!userCartRef.empty) {
-      const cartId = userCartRef.docs[0].id;
-      return res.status(200).send({ success: true, msg: "User already has a cart", cartId: cartId });
-    }
-
     const createCart = {
-      // userId: id,
-      // items,
       ...value,
     };
 
@@ -259,32 +285,55 @@ const createCartServer = async (req, res, next) => {
 };
 
 // TODO:
-// FIXME: This is not yet implemented
 // REVIEW:
 const deleteCartItemServer = async (req, res, next) => {
+  const cartId = req.params.cartId;
+  const productId = req.params.productId;
+  const productIdentifier = req.body.productIdentifier;
   try {
-    const cartId = req.params.cartId;
-    const productId = req.params.productId;
-    const productIdentifier = req.params.productIdentifier;
-
-    if (!cartId || typeof cartId !== "string" || !productId || typeof productId !== "string" || !productIdentifier || typeof productIdentifier !== "string") {
+    // NOTE: This can just be set as a helper function
+    if (!cartId || typeof cartId !== "string" || !productId || typeof productId !== "string") {
       return res.status(400).send({ success: false, msg: "Invalid ID parameter" });
     }
-    // TODO: Check if the item exists in the cart
-    // TODO: Check for nested items in the cart, etc
-    const doc = await cartCollectionRef.doc(id).get();
 
-    if (!doc.exists) {
-      return res.status(404).send({ success: false, msg: `CART NOT FOUND [SERVER]` });
-    } else {
-      const { userId, items } = doc.data();
-      const response = {
-        cartId: doc.id,
-        userId,
-        items,
-      };
-      return res.status(200).send({ success: true, data: response });
+    // NOTE: This can just be set as a helper function
+    // Check if the cart exists
+    const cartDoc = await cartCollectionRef.doc(cartId).get();
+    if (!cartDoc.exists) {
+      return res.status(404).send({ success: false, msg: "Cart not found" });
     }
+
+    // NOTE: This can just be set as a helper function
+    if (!productIdentifier || typeof productIdentifier !== "string") {
+      return res.status(400).send({ success: false, msg: "Missing product identifier" });
+    }
+
+    // Check if the product exists
+    const productDoc = await productsCollectionRef.doc(productId).get();
+    if (!productDoc.exists) {
+      await cartCollectionRef.doc(cartId).update({
+        items: admin.firestore.FieldValue.arrayRemove({ productId, productIdentifier }),
+      });
+      return res.status(200).send({ success: true, msg: "Product not found, item removed from cart" });
+    }
+
+    // Check if the item with the given productIdentifier and productId exists in the cart
+    const cartItems = cartDoc.data().items;
+    const itemIndex = cartItems.findIndex((item) => item.productId === productId && item.productIdentifier === productIdentifier);
+    if (itemIndex === -1) {
+      return res.status(404).send({ success: false, msg: "Item not found in the cart" });
+    }
+
+    // Remove the item from the cart
+    cartItems.splice(itemIndex, 1);
+    await cartCollectionRef.doc(cartId).update({ items: cartItems });
+
+    // Recalculate the total price to properly update it for the removed items
+    const updatedCartDoc = await cartCollectionRef.doc(cartId).get();
+    const updatedCartItems = updatedCartDoc.data().items;
+    const totalPrice = calculateTotalPrice(updatedCartItems);
+    await cartCollectionRef.doc(cartId).update({ totalPrice });
+    return res.status(200).send({ success: true, msg: "Item removed from cart", totalPrice: totalPrice });
   } catch (error) {
     console.error(`DELETE CART ITEM ERROR [SERVER] ${error.message}`);
     return res.status(400).send({ success: false, msg: `DELETE CART ITEM ERROR [SERVER] ${error.message}` });
@@ -292,5 +341,5 @@ const deleteCartItemServer = async (req, res, next) => {
 };
 
 module.exports = {
-  cartTestRouteServer, addToCartServer, changeCartItemQuantityServer, clearCartItemsServer, getUserCartServer, createCartServer, deleteCartItemServer,
+  cartTestRouteServer, addToCartServer, updateCartItemQuantityServer, getUserCartServer, createCartServer, deleteCartItemServer,
 };
