32b16d6a8ef9ee4622ef26cb6735a1ca734f9f67
danielevans254 <danielevans254@yahoo.com>
2024-02-04 16:18:11 +0800

    feat: Add report routes

---
+++ backend/functions/routes/api/report.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable new-cap */
+
+const router = require("express").Router();
+
+const reportController = require("../../controllers/reportController");
+
+router.get("/", reportController.reportTestRouteServer);
+
+router.get("/product/:productId", reportController.reportProductServer);
+router.get("/product/all", reportController.reportProductAllServer);
+router.get("/order/:orderId", reportController.reportOrderServer);
+router.get("/order/all", reportController.reportOrderAllServer);
+router.get("/user/:userId", reportController.reportUserServer);
+router.get("/user/all", reportController.reportUserAllServer);
+
+module.exports = router;
