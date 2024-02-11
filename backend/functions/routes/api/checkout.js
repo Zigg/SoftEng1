af58a885fdddc1ef8168474908e00919f4161497
danielevans254 <danielevans254@yahoo.com>
2024-02-11 17:56:10 +0800

    feat: add the checkout routes

--- backend/functions/routes/api/checkout.js
+++ backend/functions/routes/api/checkout.js
@@ -1,14 +1,20 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable object-curly-spacing */
-/* eslint-disable max-len */
-/* eslint-disable new-cap */
-const express = require("express");
 const router = require("express").Router();
-
+require("dotenv").config();
 const checkoutController = require("../../controllers/checkoutController");
 
 router.get("/", checkoutController.checkoutTestRouteServer);
+router.get("/:sessionId", checkoutController.getCheckoutSessionByIdServer);
+router.get("/:sessionId/line_items", checkoutController.getSessionLineItemsServer);
 
-router.post("/webhook", express.raw({ type: "application/json" }), checkoutController.webhookEvent);
+// TODO: In progress
+// TODO: Create a route for the checkout session
+router.post("/create-checkout-session", checkoutController.createCheckoutSessionServer);
+
+// NOTE: Not a webhook
+router.post("/checkout-intent", checkoutController.createCheckoutIntentServer);
+
+router.post("/create-checkout-status", checkoutController.createCheckoutStatusServer);
+
+router.post("/webhook", checkoutController.webhookEventHandler);
 
 module.exports = router;
