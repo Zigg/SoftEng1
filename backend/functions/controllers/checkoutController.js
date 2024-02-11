7a311f9a67cc9876a090b29ce70d95664c32bb46
danielevans254 <danielevans254@yahoo.com>
2024-02-11 17:43:48 +0800

    fix: checkout session allowing for creating of another checkout session even if there is already a currently active session
    feat: add new api actions, get checkout session, get checkout session line items
    
    feat: made some progress, regarding checking checkout status, and creating checkout intent not fully done, however when using the given url to checkout and not using the api action its fine.
    
    todo: review and perform more testing for all the major api actions

--- backend/functions/controllers/checkoutController.js
+++ backend/functions/controllers/checkoutController.js
@@ -1,15 +1,3 @@
-/* eslint-disable indent */
-/* eslint-disable comma-dangle */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-case-declarations */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-/* eslint-disable block-spacing */
-/* eslint-disable semi */
-/* eslint-disable brace-style */
-/* eslint-disable object-curly-spacing */
-/* eslint-disable max-len */
-
 const admin = require("firebase-admin");
 const db = admin.firestore();
 const realtimeDb = admin.database();
@@ -49,11 +37,11 @@ const webhookEventHandler = async (req, res) => {
   const paymentIntent = event.data.object
   const charge = event.data.object;
   const session = event.data.object;
+  const checkout = event.data.object
 
   switch (event.type) {
     case "charge.succeeded":
       // Get the Charge object from the event data
-      // Update your Realtime Database with the charge ID and the amount
       // TODO: Replace this with your actual Realtime Database update logic
       await realtimeDb.ref("charges_webhook").child(charge.id).set({
         chargeId: charge.id,
@@ -65,7 +53,6 @@ const webhookEventHandler = async (req, res) => {
       break;
     case "payment_intent.created":
       // Get the Payment Intent object from the event data
-      // Update your Realtime Database with the payment intent ID and the amount
       // TODO: Replace this with your actual Realtime Database update logic
       await realtimeDb.ref("payment_intents_webhook").child(paymentIntent.id).set({
         paymentIntentId: paymentIntent.id,
@@ -96,7 +83,15 @@ const webhookEventHandler = async (req, res) => {
         paymentStatus: paymentIntent.status,
         paid: paymentIntent.status === "succeeded",
       });
+      break;
+    case "payment_intent.canceled":
+      // Get the Payment Intent object from the event data
+      // Update your Realtime Database with the payment intent
       // Perform any other action you want for a successful payment intent
+      break;
+    case "checkout.session.expired":
+
+      console.log("Checkout session expired", session.id, session.payment_status, session.url);
       break;
     case "payment_intent.payment_failed":
       // Get the Payment Intent object from the event data
@@ -108,12 +103,9 @@ const webhookEventHandler = async (req, res) => {
         paid: false,
       });
       break;
-
     case "product.created":
-
       break;
     case "price.created":
-
       break;
     case "checkout.session.completed":
       // Get the Checkout Session object from the event data
@@ -156,109 +148,90 @@ const webhookEventHandler = async (req, res) => {
 
 // TODO:
 // TODO: Assign the total price to the payment intent, based on the cart doc
-// FIXME:
-// TODO: Create a check to figure out if there is already an active checkout session
+// TODO: Expire the checkout session if the amount given is exactly the same as the total price, which means the payment intent is successful, too avoid creating a payment intent for the same checkout session
+// FIXME: "status": "requires_payment_method", FROM THE RESPONSE BODY OF THE PAYMENT INTENT
+// FIXME: If the payment intent is successful, then the checkout session is successful, and must be updated for the stripe api as well as the realtime database
+// FIXME: Upon updating the checkout session payment_status successfully to "paid", the payment intent must be updated as well
+// FIXME: Disable the checkout session if the payment intent is successful, to avoid creating a payment intent for the same checkout session
+// NOTE: This will be not be needed test with the given url from the checkout session object
+// FIXME: A payment intent is created but since the status is not a part of paymentIntent object apparently(it should be don't know why its not working), i cant update the payment intent status to "succeeded", to match the checkout session status, tf?! for now i can just use the checkout session url to checkout
 const createCheckoutIntentServer = async (req, res) => {
-  // Parse the request body and extract the relevant information
   const { sessionId, totalPrice } = req.body;
 
   try {
-    // Retrieve the checkout session from the session ID
     const session = await stripe.checkout.sessions.retrieve(sessionId);
-    console.log(session, "Session Data")
 
-    // Check if the checkout session is already paid
-    if (session && session.payment_status === "paid") {
+    if (session.payment_status === "paid") {
       console.log("Checkout session is already paid.");
       res.status(409).send({ success: true, id: session.payment_intent, msg: "Checkout session is already paid for" });
       return;
     }
 
-    // Check if the checkout session already has a payment intent
-    if (session && session.payment_intent) {
-      console.log("Checkout session already has a payment intent.");
-      res.status(409).send({ success: true, id: session.payment_intent, msg: "Checkout session already has a payment intent" });
-      return;
-    }
-
-    // Check if the checkout session already has a payment intent ID
-    if (session && session.payment_intent_id) {
-      console.log("Checkout session already has a payment intent ID.");
-      res.status(409).send({ success: true, id: session.payment_intent_id, msg: "Checkout session already has a payment intent ID" });
-      return;
-    }
-
-    // Check if the checkout session is already completed
-    if (session && session.payment_status === "completed") {
-      console.log("Checkout session is already completed.");
-      res.status(409).send({ success: true, id: session.payment_intent, msg: "Checkout session is already completed" });
-      return;
-    }
-
     const customerId = session.customer;
     const customer = await stripe.customers.retrieve(customerId);
-    const { email, name } = customer;
+    const { customer_email, name } = customer;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: totalPrice,
-      currency: "usd",
-      description: "(created by Stripe CLI)",
-      payment_method_types: ["card"],
-      customer: customerId,
-      metadata: {
-        email,
-        name
-      }
-    });
-
-    console.log("Checkout payment intent created successfully. Intent ID: ", paymentIntent.id);
-
-    // Check if totalPrice is greater than or equal to the session amount_total
     if (totalPrice >= session.amount_total) {
-      session.payment_status = "paid";
-      session.payment_intent = paymentIntent.id;
-
-      await stripe.checkout.sessions.update(sessionId, {
-        payment_status: "paid",
-        payment_intent: paymentIntent.id
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: totalPrice,
+        currency: "usd",
+        description: "(created by Stripe CLI)",
+        payment_method_types: ["card"],
+        customer: customerId,
+        metadata: {
+          customer_email,
+          name,
+          sessionId,
+        },
       });
 
-      await realtimeDb.ref(`checkoutSessions/${sessionId}`).update({
+      // FIXME: Why is the status not part of the paymentIntent object??
+      await stripe.paymentIntents.update(
+        paymentIntent.id,
+        {
+          metadata: {
+            checkout_session: sessionId,
+          },
+          status: "succeeded"
+        }
+      );
+
+      await realtimeDb.ref(`checkout_sessions/${sessionId}`).update({
         session: {
-          payment_status: paymentIntent.status,
-          payment_intent: paymentIntent.id,
+          payment_status: "paid",
+          id: session.id,
         },
         paymentIntent: {
           id: paymentIntent.id,
-          status: paymentIntent.status,
           amount: paymentIntent.amount,
           currency: paymentIntent.currency,
-          description: paymentIntent.description,
           metadata: paymentIntent.metadata,
         },
       });
 
-
       res.status(200).send({ success: true, id: paymentIntent.id });
     } else {
-      res.status(400).send({ success: false, msg: "Not enough funds" })
+      res.status(400).send({ success: false, msg: "Not enough funds" });
     }
+
   } catch (error) {
     console.error(`Error creating checkout payment intent: ${error.message}`);
     res.status(500).send(error.message);
   }
 };
 
-
-// TODO:
 // TODO: Assign the total price to the checkout session, based on the cart doc, and the items, majority of the items from the request body must come from the session object
-// TODO: This works already, but the line items must come from the cart
 // TODO: Complete the check from the checkoutSchema
 // TODO: Create cart middleware to check if the user is the owner of the cart
+// TODO: Fix the customer fields to be included properly in the checkout session object
+// NOTE: To complete the checkout session just taking url from the response body of the checkout session object, then checking out via that url
+// TODO: Just checkout for the cart owner instead of assigning the userId directly to the checkout session object, this will be done later
+// NOTE: Creating 2 separate calls to the realtime database and stripe's api to create a checkout session, and then updating the checkout session in the realtime database and stripe's api with the same data might slow things down, this will be changed later
+// REVIEW: Check for an existing session that is unpaid, and if it is unpaid, then return that session and persist it, if not create a new session
+// FIXME: There are some issues with the header values 
+// ">  Error creating or retrieving checkout session: Cannot set headers after they are sent to the client"
 const createCheckoutSessionServer = async (req, res) => {
-  // Parse the request body and extract the relevant information
   const { userId, cartId } = req.body;
-  // Validate the request body using your checkout session schema
   const user = await admin.auth().getUser(userId);
   if (!user) {
     return res.status(404).send({ success: false, msg: "User not found" });
@@ -270,95 +243,141 @@ const createCheckoutSessionServer = async (req, res) => {
   const customerName = user.providerData[0].displayName || user.email;
   req.body.customerName = customerName;
   const { error, value } = checkoutSessionSchema.validate(req.body);
-  // If there is an error, return a 400 Bad Request response with the error message
   if (error) {
     console.error(`Validation error: ${error.message}`);
     return res.status(400).send(error.message);
   }
-  // TODO: Map the items from the cart to the line items
   try {
+    let session;
+    const existingCustomer = await stripe.customers.list({ id: value.customerId, limit: 1 });
     let customer;
-    const existingCustomer = await stripe.customers.list({ email: value.customerEmail, limit: 1 });
     if (existingCustomer.data.length > 0) {
       customer = existingCustomer.data[0];
     } else {
       customer = await stripe.customers.create(
         { email: value.customerEmail },
-        { idempotencyKey: value.customerEmail }
+        { idempotencyKey: value.customerId }
       );
     }
-
-    const session = await stripe.checkout.sessions.create({
-      customer_email: value.customerEmail,
-      client_reference_id: value.userId,
-      customer: customer.id,
-      payment_method_types: ["card"],
-      line_items: cartDoc.data().items.map((item) => ({
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: item.productName,
+    // Get the active sessions for the customer
+    const activeSessions = await stripe.checkout.sessions.list({ customer: customer.id });
+    // Find the active session with the same cart ID
+    const activeSession = activeSessions.data.find((s) => s.metadata.cartId === cartId);
+    console.log("Active session: ", activeSession)
+    // Check if the session is still pending payment and has a valid URL
+    if (activeSession.payment_status === "requires_payment_method" && activeSession.url) {
+      return res.status(200).json({ success: true, id: activeSession.id, url: activeSession.url });
+    }
+    if (activeSession.url !== null) {
+      session = await stripe.checkout.sessions.retrieve(activeSession.id);
+      console.log("Active checkout session found. Session ID: ", activeSession.id);
+      return res.status(409).json({ success: true, id: activeSession.id, url: activeSession.url, msg: `There is already an existing checkout session for Cart ID: ${cartId}` });
+    }
+    else {
+      session = await stripe.checkout.sessions.create({
+        customer_email: value.customerEmail,
+        client_reference_id: value.userId,
+        customer: customer.id,
+        payment_method_types: ["card"],
+        line_items: cartDoc.data().items.map((item) => ({
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: item.productName,
+            },
+            unit_amount: parseInt((item.productPrice * 100).toFixed(2)),
           },
-          unit_amount: parseInt((item.productPrice * 100).toFixed(2)),
+          quantity: item.productQuantity,
+        })),
+        mode: "payment",
+        success_url: `https://example.com/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: "https://example.com/cancel",
+        metadata: {
+          cartId,
+          userId,
         },
-        quantity: item.productQuantity,
-      })),
-      mode: "payment",
-      success_url: `https://example.com/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: "https://example.com/cancel",
+      });
+      console.log("Checkout session created successfully. Session ID: ", session.id);
+    }
+    await realtimeDb.ref(`checkout_sessions/${session.id}`).set({
+      session: {
+        payment_status: session.payment_status,
+        payment_intent: session.payment_intent,
+      },
+      paymentIntent: {
+        id: session.payment_intent,
+        status: session.payment_status,
+        amount: session.amount_total,
+        currency: session.currency,
+        description: session.description,
+        metadata: session.metadata,
+      },
     });
-    console.log("Checkout session created successfully. Session ID: ", session.id);
 
-    // TODO: Save the session data in the realtime database
-    await realtimeDb.ref(`checkout_sessions/${session.id}`).update(session);
-
-    res.status(200).json({ success: true, id: session.id });
-    res.redirect(303, session.url);
-  } catch (error) {
-    console.error(`Error creating checkout session: ${error.message}`);
-    res.status(500).send({ success: false, err: error.message });
+    res.status(200).json({ success: true, id: session.id, url: session.url });
+  }
+  catch (error) {
+    console.error(`Error creating or retrieving checkout session: ${error.message}`);
+    res.status(500).send(error.message);
   }
 };
+
 
 // TODO: Set as an API Action instead of a webhook
 // TODO: Create more validations
 // FIXME: Some fields may actually be null if a payment intent is not yet been made
+// FIXME: The payment intent id is not yet been made, so it is null, and may be causing issues, this will be fixed later
 const createCheckoutStatusServer = async (req, res, next) => {
   const { sessionId } = req.body;
 
   try {
     const session = await stripe.checkout.sessions.retrieve(sessionId);
-    const checkoutSessionRef = realtimeDb.ref(`checkout_sessions/${sessionId}`);
+    const paymentIntentId = session?.payment_intent;
 
-    // Retrieve the payment status from the Realtime Database
-    const snapshot = await checkoutSessionRef.once("value");
-    const paymentStatus = snapshot.val()?.payment_status;
-    // console.log(session.payment_intent?.id, "Session Payment Intent ID");
-    // console.log(paymentStatus, "Payment Status");
-    // console.log(session.payment_intent?.status, "Session Payment Intent Status");
+    if (!paymentIntentId) {
+      return res.status(200).send({ success: false, msg: `No payment intent has been made for Session: ${sessionId}` });
+    }
 
-    if (
-      (session.payment_status === "paid" || session.payment_intent.status === "succeeded") &&
-      (paymentStatus === "paid" || paymentStatus === "succeeded")
-    ) {
+    const paymentIntent = await stripe.paymentIntents?.retrieve(paymentIntentId);
+    console.log(session, "Session Data")
+
+    if ((session.payment_status === "paid" || paymentIntent?.status === "succeeded") && session.status === "complete" && session.url === null) {
       return res.status(200).send({ success: true, msg: "Checkout session completed" });
-    } else if (
-      (session.payment_status === "unpaid" || session.payment_intent.status === "requires_payment_method") &&
-      (paymentStatus === "unpaid" || paymentStatus === "requires_payment_method")
-    ) {
+    } else if (session.payment_status === "unpaid" || paymentIntent?.status === "requires_payment_method") {
       return res.status(200).send({ success: false, msg: "Checkout session unpaid" });
     } else {
       return res.status(400).send("Invalid payment status");
     }
   } catch (error) {
-    console.error(`CHECKOUT SESSION ERROR [SERVER] ${error.message}`);
+    console.error(`GET CHECKOUT STATUS [SERVER] ${error.message}`);
     return res.status(500).send({
       success: false,
-      msg: `CHECKOUT SESSION ERROR [SERVER] ${error.message}`,
+      msg: `GET CHECKOUT STATUS [SERVER] ${error.message}`,
     });
   }
 }
 
+const getCheckoutSessionByIdServer = async (req, res, next) => {
+  const sessionId = req.params.sessionId;
+  try {
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    return res.status(200).send({ success: true, data: session });
+  } catch (error) {
+    return res.status(500).send({ success: false, msg: `GET SESSION BY ID ERROR [SERVER] ${error.message}` });
+  }
+};
+
+const getSessionLineItemsServer = async (req, res, next) => {
+  const sessionId = req.params.sessionId;
+  try {
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    const lineItems = await stripe.checkout.sessions.listLineItems(sessionId);
+    return res.status(200).send({ success: true, data: lineItems });
+  } catch (error) {
+    return res.status(500).send({ success: false, msg: `GET SESSION LINE ITEMS ERROR [SERVER] ${error.message}` });
+  }
+};
+
 module.exports = {
-  createCheckoutIntentServer, checkoutTestRouteServer, webhookEventHandler, createCheckoutStatusServer, createCheckoutSessionServer,
+  createCheckoutIntentServer, checkoutTestRouteServer, webhookEventHandler, createCheckoutStatusServer, createCheckoutSessionServer, getCheckoutSessionByIdServer, getSessionLineItemsServer,
 }
