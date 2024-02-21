b104f9a0d1d94d14d502e8a9a21663f9d986ee4c
danielevans254 <danielevans254@yahoo.com>
2024-02-21 17:50:36 +0800

    feat: update report api routes

--- backend/functions/routes/api/report.js
+++ backend/functions/routes/api/report.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const reportController = require("../../controllers/reportController");
-
+const checkoutReportController = require("../../controllers/reports/checkoutSessionReportController");
+const orderReportController = require("../../controllers/reports/orderReportController");
 router.get("/", reportController.reportTestRouteServer);
+const revenueReportController = require("../../controllers/reports/revenueReportController");
 
 router.get("/product/:productId", reportController.reportProductServer);
 router.get("/product/all", reportController.reportProductAllServer);
@@ -10,5 +12,11 @@ router.get("/order/:orderId", reportController.reportOrderServer);
 router.get("/order/all", reportController.reportOrderAllServer);
 router.get("/user/:userId", reportController.reportUserServer);
 router.get("/user/all", reportController.reportUserAllServer);
+
+router.get("/gross-revenue", revenueReportController.grossRevenueReportServer);
+
+router.get("/checkout-session-completed", checkoutReportController.completedCheckoutSessionReportServer);
+
+router.get("/order-status", orderReportController.orderStatusReportServer);
 
 module.exports = router;
