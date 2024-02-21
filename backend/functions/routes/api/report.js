f3cac7f8923eb9c302d4bb4a4c2bba105cb141b0
danielevans254 <danielevans254@yahoo.com>
2024-02-21 17:51:59 +0800

    fix: update folder structure and api reports routes

--- backend/functions/routes/api/report.js
+++ backend/functions/routes/api/report.js
@@ -1,17 +1,8 @@
 const router = require("express").Router();
 
-const reportController = require("../../controllers/reportController");
 const checkoutReportController = require("../../controllers/reports/checkoutSessionReportController");
 const orderReportController = require("../../controllers/reports/orderReportController");
-router.get("/", reportController.reportTestRouteServer);
 const revenueReportController = require("../../controllers/reports/revenueReportController");
-
-router.get("/product/:productId", reportController.reportProductServer);
-router.get("/product/all", reportController.reportProductAllServer);
-router.get("/order/:orderId", reportController.reportOrderServer);
-router.get("/order/all", reportController.reportOrderAllServer);
-router.get("/user/:userId", reportController.reportUserServer);
-router.get("/user/all", reportController.reportUserAllServer);
 
 router.get("/gross-revenue", revenueReportController.grossRevenueReportServer);
 
