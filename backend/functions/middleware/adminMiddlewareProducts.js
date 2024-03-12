0422d6ed68de042d8cd02f9d54f5c91fc92d47b6
danielevans254 <danielevans254@yahoo.com>
2024-03-12 14:17:23 +0800

    feat: simplified how to check for an admin, by just fetching the uuid input into the auth headers and if that user is an admin, allow admin permissions

--- backend/functions/middleware/adminMiddlewareProducts.js
+++ backend/functions/middleware/adminMiddlewareProducts.js
@@ -1,32 +1,32 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-// Middleware to check if the user is an admin. 
 const checkAdminRole = async (req, res, next) => {
-    try {
-        const idToken = req.headers.authorization || '';
-    
-        if (!idToken) {
-            return res.status(401).send({ success: false, msg: "Authorization required" });
-        }
-    
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
-    
-        const user = await admin.auth().getUser(decodedToken.uid);
-    
-        if (!user) {
-            return res.status(404).send({ success: false, msg: "User not found" });
-        }
-    
-        if (user.customClaims && user.customClaims.admin === true) {
-            return next();
-        }
-    
-        return res.status(403).send({ success: false, msg: "You are not authorized!" });
-    } catch (error) {
-      res.status(403).send({ success: false, msg: "You are not authorized" });
+  try {
+    // NOTE: Assigning the UID to req.headers.authorization for testing, this will later be changed to a bearer token returned by firestore, I will create another endpoint to create an valid api key that will be assigned certain permissions based on the role of the user and the permissions set by the user.
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader && authorizationHeader.split(" ")[1];
+    const uid = token;
+
+    if (!uid) {
+      return res.status(401).send({ success: false, msg: "UID required for Authorization" });
     }
+
+    const user = await admin.auth().getUser(uid);
+
+    if (!user) {
+      return res.status(404).send({ success: false, msg: "User not found" });
+    }
+
+    if (user.customClaims && user.customClaims.admin === true) {
+      return next();
+    }
+
+    return res.status(403).send({ success: false, msg: "You are not authorized!" });
+  } catch (error) {
+    res.status(403).send({ success: false, msg: "You are not authorized" });
+  }
 };
 
 module.exports = {
-     checkAdminRole 
+  checkAdminRole,
 };
