843dec6ab89bf1202452e9850ef49ca06875123d
danielevans254 <danielevans254@yahoo.com>
2024-03-14 18:32:35 +0800

    fix: add the error types and handling of the jwt token errors

--- backend/functions/helpers/tokens/tokenActions.js
+++ backend/functions/helpers/tokens/tokenActions.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 const jwtWebTokenSecretKey = process.env.JWT_WEB_TOKEN_SECRET_KEY;
 const tokenSchema = require("../../models/tokenModel");
 const fetchRole = require("../fetchRole");
-
 const generateToken = async (userId) => {
   const firebaseUserId = await admin.auth().getUser(userId);
   const uid = firebaseUserId.uid;
@@ -34,16 +33,25 @@ const generateToken = async (userId) => {
 };
 
 // TODO: Create the middleware for any route that requires a token to be verified, later to be added down the line, check also the customClaims for the user to see if they are an admin or not.
-const verifyToken = async (token) => {
+const verifyToken = async (token, res, req) => {
   try {
     const decodedToken = jwt.verify(token, jwtWebTokenSecretKey);
     const { error } = tokenSchema.validate(decodedToken);
     if (error) {
-      return new Error("Invalid token");
+      throw new Error("Invalid token");
     }
     return decodedToken.userId;
   } catch (error) {
-    return null;
+    if (error.name === "TokenExpiredError") {
+      res.status(401).send({ success: false, msg: "Token expired" });
+    } else if (error.name === "NotBeforeError") {
+      res.status(401).send({ success: false, msg: "Token not yet valid" });
+    } else if (error.name === "InvalidTokenError") {
+      res.status(401).send({ success: false, msg: "The header or payload could not be parsed" });
+    } else {
+      console.error("Error:", error);
+      return null;
+    }
   }
 };
 
