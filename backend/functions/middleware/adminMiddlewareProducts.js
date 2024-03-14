c5b225f065d2c621c4a840739a927fc4736da383
danielevans254 <danielevans254@yahoo.com>
2024-03-14 18:31:10 +0800

    fix: separation of concerns for checking token and checking admin role

--- backend/functions/middleware/adminMiddlewareProducts.js
+++ backend/functions/middleware/adminMiddlewareProducts.js
@@ -2,7 +2,8 @@ const admin = require("firebase-admin");
 const db = admin.firestore();
 const tokensCollectionRef = db.collection("tokens");
 const { verifyToken } = require("../helpers/tokens/tokenActions");
-const checkAdminRole = async (req, res, next) => {
+
+const checkToken = async (req, res) => {
   try {
     const authorizationHeader = req.headers.authorization;
     const token = authorizationHeader && authorizationHeader.split(" ")[1];
@@ -10,29 +11,34 @@ const checkAdminRole = async (req, res, next) => {
     if (!token) {
       return res.status(401).send({ success: false, msg: "Token required for Authorization" });
     }
-    const verifiedToken = await verifyToken(token);
-    console.log("verifiedToken", verifiedToken);
+
+    const verifiedToken = await verifyToken(token, res, req);
     const tokenDoc = await tokensCollectionRef.doc(token).get();
 
-    if (!tokenDoc.exists) {
+    if (!tokenDoc.exists && !verifiedToken) {
       return res.status(404).send({ success: false, msg: "Token not found" });
     }
 
-    const tokenData = tokenDoc.data();
+    return tokenDoc.data();
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(403).send({ success: false, msg: "You are not authorized" });
+  }
+};
 
-    // NOTE: Check if the token is expired, this is not yet final
-    if (tokenData.expires < Date.now()) {
-      return res.status(401).send({ success: false, msg: "Token expired" });
-    }
+const checkAdminRole = async (req, res, next) => {
+  try {
+    const tokenData = await checkToken(req, res);
 
-    // TODO: Cache the role value
+    // TODO: Cache the role value, and check if the token is still valid, use an in memory store, maybe
     const role = tokenData.role;
 
     if (role === "admin") {
       return next();
     }
-    return res.status(403).send({ success: false, msg: "You are not authorized!", token: tokenData });
+    return res.status(403).send({ success: false, msg: "You are not authorized!" });
   } catch (error) {
+    console.error("Error:", error);
     res.status(403).send({ success: false, msg: "You are not authorized" });
   }
 };
